4cef020491dec407fc106d33f908f7cd66e32fab
ameen <ameenazeemi@live.com>
2018-06-21 17:57:29 +0500

    saving

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-
+declare var cmbScanner:any; 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
@@ -8,7 +8,28 @@ import { NavController } from 'ionic-angular';
 export class HomePage {
 
   constructor(public navCtrl: NavController) {
-
+    //mwbScanner.setCallback(function(result){});
   }
+
+  goScan()
+  {
+    debugger;
+
+    if(typeof cmbScanner != 'undefined' && typeof cmbScanner.startScanning != 'undefined'){
+
+    alert("Scan click");
+    cmbScanner.startScanning().then(function(response){
+      console.log('show the result here');
+      console.log(response);
+      //actual example in home.ts is different
+    });
+  }
+  else
+  {
+    alert("Not Defined");
+  }
+
+
+  };
 
 }
